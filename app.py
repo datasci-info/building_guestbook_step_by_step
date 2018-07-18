455b18372318448448752f4c7e192287bd20bf34
Chia-Chi Chang <c3h3.tw@gmail.com>
2018-07-18 10:01:35 +0800

    update livedemo code

--- app.py
+++ app.py
@@ -9,16 +9,65 @@ try:
 except:
     pass
 
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort, render_template,redirect, url_for 
 
 app = Flask(__name__)
+
+# Variables about mongodb 
+import os
+MONGODB_HOST = os.environ.get("MONGODB_HOST", "54.236.77.163") 
+MONGODB_PORT = int(os.environ.get("MONGODB_PORT", 27017)) 
+MONGODB_USER = os.environ.get("MONGODB_USER", "root") 
+MONGODB_PWD = os.environ.get("MONGODB_PWD", "example") 
+DB = os.environ.get("DB", "admin") 
+
+
+try: 
+    from local_settings import * 
+except: 
+    pass 
+
+from pymongo import MongoClient 
+
+mcli = MongoClient(host=MONGODB_HOST, port=MONGODB_PORT) 
+db = mcli[DB] 
+
+  
+if MONGODB_USER!="": 
+    db.authenticate(MONGODB_USER,MONGODB_PWD) 
+    try:
+        db.create_collection("guestbook") 
+    except Exception as e:
+        print("error = ",e)
+
+  
+
+db = mcli["guestbook"] 
+
+sample_data = [
+    {"text":"message1"},
+    {"text":"message2"},
+    {"text":"message3"},
+    {"text":"message4"},
+]
+
+if db.messages.find().count() == 0:
+    db.messages.insert(sample_data)
+
+
 
 @app.route('/')
 def index():
     return render_template("home.html",
         title = 'Home',
-        app_name = APP_NAME)
+        app_name = APP_NAME,
+        data = list(db.messages.find()))
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    raw = {"text":request.form['message'],"author":request.form['name']}
+    db.messages.insert(raw)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.debug = DEBUG
